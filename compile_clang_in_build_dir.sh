d3cf0f589c740c59937bce1b75de284dfd7850a3
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2020-05-10 18:11:19 +0300

    Set ComputeCpp_DIR

--- compile_clang_in_build_dir.sh
+++ compile_clang_in_build_dir.sh
@@ -4,6 +4,11 @@ mkdir -p build
 cp *.hmm build/
 cp *.fsa build/
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ ..
+cmake \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DComputeCpp_DIR=/usr/local/ComputeCpp-CE-2.0.0-x86_64-linux-gnu/ \
+..
 make -j 3
 cd ..

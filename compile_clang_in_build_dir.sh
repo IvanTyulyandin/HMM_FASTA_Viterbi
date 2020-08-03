e3b8e7e5d9a8fec74d303a044537ad7a0b9d1dde
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2020-08-03 18:35:52 +0300

    Use ComputeCpp 2.1.0

--- compile_clang_in_build_dir.sh
+++ compile_clang_in_build_dir.sh
@@ -8,7 +8,7 @@ cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_CXX_COMPILER=clang++ \
-    -DComputeCpp_DIR=/usr/local/ComputeCpp-CE-2.0.0-x86_64-linux-gnu/ \
+    -DComputeCpp_DIR=/usr/local/ComputeCpp-CE-2.1.0-x86_64-linux-gnu/ \
 ..
 make -j 3
 cd ..

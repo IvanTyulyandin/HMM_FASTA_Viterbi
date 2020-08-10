a5de5e7570d166e1039e7dba4ba688e95244d2a1
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2020-08-10 23:07:17 +0300

    Make MSV_p_gen process many HMMs

--- algorithms/MSV_p_gen.py
+++ algorithms/MSV_p_gen.py
@@ -3,25 +3,17 @@ import sys
 import os
 import fileinput
 
-model_length: int = 0
-NUM_OF_AMINO_ACIDS: int = 20
-
-hmm_file: str = sys.argv[1]
-hmm_name: str = hmm_file[:-4]
-slash_index: int = hmm_name.rfind('/')
-if slash_index != -1:
-    hmm_name = hmm_name[slash_index+1:]
-
-# emulate M0
-match_emissions: list = [[0] * NUM_OF_AMINO_ACIDS]
-
 
 def neg_ln_to_prob(prob_inside_ln: float):
     return math.exp(-1 * prob_inside_ln)
 
 
-cpp_MSV_initial = f'''#include \"MSV_HMM_spec.hpp\"
+model_length: int = 0
+NUM_OF_AMINO_ACIDS: int = 20
+nu: float = 2.0
 
+cpp_MSV_initial = f'''#include \"MSV_HMM_spec.hpp\"
+    
 #include <CL/sycl.hpp>
 #include <iostream>
 #include <unordered_map>
@@ -34,26 +26,6 @@ const auto amino_acid_num = std::unordered_map<char, int>{{
 
 '''
 
-with open(hmm_file, 'r') as hmm:
-    while True:
-        line: str = hmm.readline().lstrip()
-        if line.startswith('COMPO'):
-            break
-        if line.startswith('LENG'):
-            model_length = int(line.split()[1])
-
-    hmm.readline()
-    hmm.readline()
-    line: str = hmm.readline().strip()
-    while line != '//':
-        data: list = line.split()[1:(NUM_OF_AMINO_ACIDS + 1)]
-        match_emissions.append(list(map(lambda d: neg_ln_to_prob(float(d)), data)))
-        hmm.readline()
-        hmm.readline()
-        line = hmm.readline().strip()
-
-# take into account dummy node M0
-model_length += 1
 background_frequencies: list = [
     0.0787945, 0.0151600, 0.0535222, 0.0668298,  # A C D E
     0.0397062, 0.0695071, 0.0229198, 0.0590092,  # F G H I
@@ -62,53 +34,84 @@ background_frequencies: list = [
     0.0540687, 0.0673417, 0.0114135, 0.0304133   # T V W Y
 ]
 
-emissions_scores: list = [0] * NUM_OF_AMINO_ACIDS * model_length
+# generate specialized code
+for hmm_file in sys.argv[1:]:
+    hmm_name: str = hmm_file[:-4]
+    slash_index: int = hmm_name.rfind('/')
+    if slash_index != -1:
+        hmm_name = hmm_name[slash_index+1:]
 
-for i in range(model_length):
-    for j in range(NUM_OF_AMINO_ACIDS):
-        if match_emissions[i][j] == 0:
-            emissions_scores[j * model_length + i] = -math.inf
-        else:
-            log_score: float = math.log(match_emissions[i][j] / background_frequencies[j])
-            emissions_scores[j * model_length + i] = log_score
+    # emulate M0
+    match_emissions: list = [[0] * NUM_OF_AMINO_ACIDS]
 
+    # read match_emissions
+    with open(hmm_file, 'r') as hmm:
+        while True:
+            line: str = hmm.readline().lstrip()
+            if line.startswith('COMPO'):
+                break
+            if line.startswith('LENG'):
+                model_length = int(line.split()[1])
 
-cpp_emissions_scores_initializer: str = '{\n'
-for i in range(model_length):
-    cpp_emissions_scores_initializer += '      '
-    stride: int = i * NUM_OF_AMINO_ACIDS
-    cpp_emissions_scores_initializer += ', '.join(map(lambda f: '%.6f' % f, emissions_scores[stride:stride+20])) + ',\n'
-cpp_emissions_scores_initializer += '    }'
+        hmm.readline()
+        hmm.readline()
+        line: str = hmm.readline().strip()
+        while line != '//':
+            data: list = line.split()[1:(NUM_OF_AMINO_ACIDS + 1)]
+            match_emissions.append(list(map(lambda d: neg_ln_to_prob(float(d)), data)))
+            hmm.readline()
+            hmm.readline()
+            line = hmm.readline().strip()
 
-nu: float = 2.0
-# constants calculated from model_length
-B_Mk_score: str = 'static_cast<float>(' + str(math.log(2.0 / (model_length * (model_length + 1)))) + ')'
-E_C_score: str = 'static_cast<float>(' + str(math.log((nu - 1.0) / nu)) + ')'
-E_J_score: str = 'static_cast<float>(' + str(math.log(1.0 / nu)) + ')'
+    # take into account dummy node M0
+    model_length += 1
 
-E: int = model_length
-J: int = model_length + 1
-C: int = model_length + 2
-N: int = model_length + 3
-B: int = model_length + 4
-rows: int = 2
-cols: int = model_length + 5
+    emissions_scores: list = [0] * NUM_OF_AMINO_ACIDS * model_length
 
-num_of_real_M_states: int = model_length - 1
-should_use_M0: int = num_of_real_M_states % 2 != 0
-max_M_buf_size: int = num_of_real_M_states + should_use_M0
+    for i in range(model_length):
+        for j in range(NUM_OF_AMINO_ACIDS):
+            if match_emissions[i][j] == 0:
+                emissions_scores[j * model_length + i] = -math.inf
+            else:
+                log_score: float = math.log(match_emissions[i][j] / background_frequencies[j])
+                emissions_scores[j * model_length + i] = log_score
 
-cpp_newline = "\'\\n\'"
-init_dp: str = f'init_dp_spec_{hmm_name}'
-init_N_B: str = f'init_N_B_spec_{hmm_name}'
-M_states_handler: str = f'M_states_handler_spec_{hmm_name}'
-copy_M: str = f'copy_M_spec_{hmm_name}'
-reduction_step: str = f'reduction_step_spec_{hmm_name}'
-E_J_C_N_B_states_handler: str = f'E_J_C_N_B_states_handler_spec_{hmm_name}'
+    # embed emissions_scores
+    cpp_emissions_scores_initializer: str = '{\n'
+    for i in range(model_length):
+        cpp_emissions_scores_initializer += '      '
+        stride: int = i * NUM_OF_AMINO_ACIDS
+        cpp_emissions_scores_initializer += ', '.join(map(lambda f: '%.6f' % f, emissions_scores[stride:stride+20])) + ',\n'
+    cpp_emissions_scores_initializer += '    }'
 
-# TODO: unroll while
+    # constants calculated from model_length
+    B_Mk_score: str = 'static_cast<float>(' + str(math.log(2.0 / (model_length * (model_length + 1)))) + ')'
+    E_C_score: str = 'static_cast<float>(' + str(math.log((nu - 1.0) / nu)) + ')'
+    E_J_score: str = 'static_cast<float>(' + str(math.log(1.0 / nu)) + ')'
 
-p_gen = f'''// MSV specialized at HMM from {hmm_file}
+    E: int = model_length
+    J: int = model_length + 1
+    C: int = model_length + 2
+    N: int = model_length + 3
+    B: int = model_length + 4
+    rows: int = 2
+    cols: int = model_length + 5
+
+    num_of_real_M_states: int = model_length - 1
+    should_use_M0: int = num_of_real_M_states % 2 != 0
+    max_M_buf_size: int = num_of_real_M_states + should_use_M0
+
+    cpp_newline = "\'\\n\'"
+    init_dp: str = f'init_dp_spec_{hmm_name}'
+    init_N_B: str = f'init_N_B_spec_{hmm_name}'
+    M_states_handler: str = f'M_states_handler_spec_{hmm_name}'
+    copy_M: str = f'copy_M_spec_{hmm_name}'
+    reduction_step: str = f'reduction_step_spec_{hmm_name}'
+    E_J_C_N_B_states_handler: str = f'E_J_C_N_B_states_handler_spec_{hmm_name}'
+
+    # TODO: unroll while
+
+    p_gen = f'''// MSV specialized at HMM from {hmm_file}
 // specialized SYCL kernel names
 class {init_dp};
 class {init_N_B};
@@ -240,21 +243,21 @@ Log_score MSV_HMM_spec::parallel_run_on_sequence_{hmm_name}(const Protein_sequen
     return 0;
 }}\n\n'''
 
-# Write out result of p_gen
-out_file_name = 'MSV_HMM_spec'
-out_file_name_cpp = out_file_name + '.cpp'
-out_file_name_header = out_file_name + '.hpp'
+    # Write out result of p_gen
+    out_file_name = 'MSV_HMM_spec'
+    out_file_name_cpp = out_file_name + '.cpp'
+    out_file_name_header = out_file_name + '.hpp'
 
-if os.path.exists(out_file_name_cpp) and os.stat(out_file_name_cpp).st_size > 0:
-    with open(out_file_name_cpp, 'a') as out:
-        out.write(p_gen)
-else:
-    with open(out_file_name_cpp, 'w') as out:
-        out.write(cpp_MSV_initial)
-        out.write(p_gen)
+    if os.path.exists(out_file_name_cpp) and os.stat(out_file_name_cpp).st_size > 0:
+        with open(out_file_name_cpp, 'a') as out:
+            out.write(p_gen)
+    else:
+        with open(out_file_name_cpp, 'w') as out:
+            out.write(cpp_MSV_initial)
+            out.write(p_gen)
 
-cpp_MSV_header = f'''#pragma once
-
+    cpp_MSV_header = f'''#pragma once
+    
 #include "FASTA_protein_sequences.hpp"
 
 using Log_score = float;
@@ -265,13 +268,13 @@ class MSV_HMM_spec {{
 }};
 '''
 
-# May be there is more elegant way to replace last string in file
-if os.path.exists(out_file_name_header) and os.stat(out_file_name_header).st_size > 0:
-    with fileinput.input(files=out_file_name_header, inplace=True) as f:
-        for line in f:
-            if '};' in line:
-                line = f'    static Log_score parallel_run_on_sequence_{hmm_name}(const Protein_sequence& seq);\n' + line
-            sys.stdout.write(line)
-else:
-    with open(out_file_name_header, 'w') as out:
-        out.write(cpp_MSV_header)
+    # May be there is more elegant way to replace last string in file
+    if os.path.exists(out_file_name_header) and os.stat(out_file_name_header).st_size > 0:
+        with fileinput.input(files=out_file_name_header, inplace=True) as f:
+            for line in f:
+                if '};' in line:
+                    line = f'    static Log_score parallel_run_on_sequence_{hmm_name}(const Protein_sequence& seq);\n' + line
+                sys.stdout.write(line)
+    else:
+        with open(out_file_name_header, 'w') as out:
+            out.write(cpp_MSV_header)

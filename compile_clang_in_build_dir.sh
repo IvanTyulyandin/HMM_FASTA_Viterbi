17afc787d9a471aadff28695a2089efb00eec6d8
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2020-08-12 15:44:13 +0300

    Use release mode

--- compile_clang_in_build_dir.sh
+++ compile_clang_in_build_dir.sh
@@ -11,7 +11,7 @@ cp -u -r profile_HMMs build/
 cp *.fsa build/
 cd build
 cmake \
-    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_CXX_COMPILER=clang++ \
     -DComputeCpp_DIR=/usr/local/ComputeCpp-CE-2.1.0-x86_64-linux-gnu/ \

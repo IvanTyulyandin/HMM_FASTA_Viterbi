8fc25f88f24896e1d6290a21987ad1ebd7cd39d9
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2020-08-17 12:29:12 +0300

    Check FASTA format correctness

--- data_readers/FASTA_protein_sequences.cpp
+++ data_readers/FASTA_protein_sequences.cpp
@@ -2,6 +2,9 @@
 
 #include <fstream>
 #include <iostream>
+#include <unordered_set>
+#include <algorithm>
+
 
 FASTA_protein_sequences::FASTA_protein_sequences(const std::string& file_path) {
     auto file = std::ifstream(file_path);
@@ -19,6 +22,23 @@ FASTA_protein_sequences::FASTA_protein_sequences(const std::string& file_path) {
             sequences.back() += line;
         }
     }
+
+    auto allowed_symbols = std::unordered_set<char>(
+        {'#', 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'});
+
+    sequences.erase(
+        std::remove_if(sequences.begin(), sequences.end(),
+            [&](Protein_sequence& protein) {
+                for (auto amino_acid : protein) {
+                    if (allowed_symbols.find(amino_acid) == allowed_symbols.end()) {
+                        std::cout << "Warning: sequence " << protein << " was rejected.\nReason: prohibited symbol "
+                            << amino_acid << " in " << file_path << " FASTA file\n\n";
+                        return true;
+                    }
+                }
+                return false;
+            }),
+        sequences.end());
 
     file.close();
 }

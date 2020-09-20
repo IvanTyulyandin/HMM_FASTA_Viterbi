1a757ab25ba1950d4f8f737f8c82758cc89fc00e
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2020-09-21 00:45:26 +0300

    Prepare workaround for OpenCL

--- compile_clang_in_build_dir.sh
+++ compile_clang_in_build_dir.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cd algorithms
-cp /dev/null MSV_HMM_spec.cpp
-cp /dev/null MSV_HMM_spec.hpp
-python3 MSV_p_gen.py $(find ../profile_HMMs -maxdepth 1 -type f -name "*.hmm")
-cd ..
-
 mkdir -p build
 cp -u -r profile_HMMs build/
 cp -u -r FASTA_files build/
@@ -14,7 +8,6 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_CXX_COMPILER=clang++ \
-    -DComputeCpp_DIR=/usr/local/ComputeCpp-CE-2.1.0-x86_64-linux-gnu/ \
     ..
 make -j 3
 cd ..
